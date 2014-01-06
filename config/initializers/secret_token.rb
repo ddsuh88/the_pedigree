7614d740f269f1a17b3fc272b05a8bd86f0b8423
ddsuh88 <ddsuh88@gmail.com>
2014-01-06 04:07:36 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThePedigree::Application.config.secret_key_base = '851ee2c126492443e6d5af885e75ce0956e3b9bd0948ffd018ade2c88724ae7e9c390653f971f3ce1d2a94bd4918b1d9dff4df74755ab51d9d10af0cebcf72e6'
